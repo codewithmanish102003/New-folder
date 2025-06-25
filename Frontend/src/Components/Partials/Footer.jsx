5ff1874c0b980356ce9213ee3a2b31d9a7655cc2
Manish Prajapati <marveluniverse1942@gmail.com>
2025-06-25 15:18:25 +0530

    Fix closing tag for developer credits in Footer component

--- Frontend/src/Components/Partials/Footer.jsx
+++ Frontend/src/Components/Partials/Footer.jsx
@@ -120,7 +120,7 @@ const Footer = () => {
       {/* Footer Bottom */}
       <div className="mt-8 border-t border-gray-700 pt-4 text-center text-sm text-gray-400">
         <p>© 2025 starwayCollections. All rights reserved.</p>
-<h4>Developed By : Naveen Kumawat & Manish Prajapati<h4/>
+        <h4>Developed By : Naveen Kumawat & Manish Prajapati</h4>
 
       </div>
     </footer>
